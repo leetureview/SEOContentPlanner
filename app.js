1dcade1cb7e517c7a4b39361479bb43be4adf9a6
leetureview <leetureview@gmail.com>
2026-02-10 17:25:09 +0700

    fix: Increase maxOutputTokens to 8192 to prevent truncation

--- app.js
+++ app.js
@@ -1369,7 +1369,7 @@ async function processKeywordWithAI(keyword, template, apiKey) {
             }],
             generationConfig: {
                 temperature: 0.7,
-                maxOutputTokens: 4096
+                maxOutputTokens: 8192
             }
         })
     });
